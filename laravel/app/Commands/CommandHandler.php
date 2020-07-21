8009deb6d6752968e48b12397c6a667617c4c22c
Thorrdu <thierry.hansens@gmail.com>
2020-07-21 02:37:07 +0200

    command log

--- laravel/app/Commands/CommandHandler.php
+++ laravel/app/Commands/CommandHandler.php
@@ -3,16 +3,44 @@
 namespace App\Commands;
 
 use Illuminate\Database\Eloquent\Model;
+use \Discord\Parts\Channel\Message as Message;
+use App\CommandLog as CommandLog;
 
 class CommandHandler
 {
+    public $name;
     public $message;
     public $args;
     public $discord;
 
-    public function __toString()
+    public function __construct()
     {
-        return $this->name;
+        $arguments = func_get_args();
+        $numberOfArguments = func_num_args();
+
+        if (method_exists($this, $function = '__construct'.$numberOfArguments)) {
+            call_user_func_array(array($this, $function), $arguments);
+        }
+    }
+
+    //CLI VERSION
+    public function __construct1(array $args) {
+        $this->message = null; //Factory message?
+        $this->args = $args;
+    }
+
+    //BASIC CALL
+    public function __construct2(Message $message,array $args) {
+        $this->message = $message;
+        $this->args = $args;
+    }
+
+    public function log()
+    {
+        $log = new CommandLog;
+        $log->type = $this->name;
+        $log->raw = $this->message->content;
+        $log->save();
     }
 
     public function help()
--- laravel/app/commandLog.php
+++ laravel/app/commandLog.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class commandLog extends Model
+class CommandLog extends Model
 {
     //
 }
