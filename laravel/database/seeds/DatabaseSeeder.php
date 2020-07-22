d43ff6d45ca1010411dfdfe8ee4b00450088b938
Thorrdu <thierry.hansens@gmail.com>
2020-07-23 00:40:37 +0200

    New Buildings and Technologies

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
             'quartz' => 0,
             'naqahdah' => 0,
             'production_type' => 'special',
-            'production_base' => 100,
+            'production_base' => 200,
             'production_coefficient' => 1.2,
             'energy_base' => NULL,
             'energy_coefficient' => NULL,
@@ -157,7 +157,197 @@ class DatabaseSeeder extends Seeder
             'time_base' => 648,
             'time_coefficient' => 1.7
         ]);
+
+        DB::table('buildings')->insert([
+            'id' => 8,
+            'name' => 'Station de clônage',
+            'description' => "Permet de recruter de nouvelles troupes et ingérnieurs",
+            'type' => 'Military',
+            'iron' => 500,
+            'gold' => 500,
+            'quartz' => 0,
+            'naqahdah' => 0,
+            'production_type' => 'military',
+            'production_base' => null,
+            'production_coefficient' => null,
+            'energy_base' => null,
+            'energy_coefficient' => null,
+            'display_order' => 1,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1200,
+            'time_coefficient' => 1.7
+        ]);
+
+        DB::table('buildings')->insert([
+            'id' => 9,
+            'name' => 'Chantier Spacial',
+            'description' => "Permet de développer la construction de sondes et vaisseaux spaciaux",
+            'type' => 'Military',
+            'iron' => 100,
+            'gold' => 200,
+            'quartz' => 100,
+            'naqahdah' => 0,
+            'production_type' => 'military',
+            'display_order' => 1,
+            'upgrade_coefficient' => 1.9,
+            'time_base' => 1140,
+            'time_coefficient' => 1.7
+        ]);
+
+        DB::table('buildings')->insert([
+            'id' => 10,
+            'name' => 'Reacteur au Naqahdah',
+            'description' => "Centrale permettant de générer d'énormes quantités d'énergie en consommant du Naqahdah.",
+            'type' => 'Energy',
+            'iron' => 5200,
+            'gold' => 3000,
+            'quartz' => 750,
+            'naqahdah' => 0,
+            'production_type' => 'special',
+            'production_base' => 300,
+            'production_coefficient' => 1.3,
+            'energy_base' => NULL,
+            'energy_coefficient' => NULL,
+            'display_order' => 5,
+            'upgrade_coefficient' => 1.6,
+            'time_base' => 1800
+        ]);
+
+        DB::table('buildings')->insert([
+            'id' => 11,
+            'name' => 'Entrepôt de fer',
+            'description' => "Permet d'augmenter la capacité de stockage en Fer de 1.8 / LVL",
+            'type' => 'Storage',
+            'iron' => 2000,
+            'gold' => 0,
+            'quartz' => 0,
+            'naqahdah' => 0,
+            'production_type' => 'iron',
+            'production_coefficient' => 1.8,
+            'energy_base' => NULL,
+            'energy_coefficient' => NULL,
+            'display_order' => 1,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1800,
+            'time_coefficient' => 1.5
+        ]);
+
+        DB::table('buildings')->insert([
+            'id' => 12,
+            'name' => 'Entrepôt d\'Or',
+            'description' => "Permet d'augmenter la capacité de stockage en Or de 1.8 / LVL",
+            'type' => 'Storage',
+            'iron' => 2000,
+            'gold' => 1000,
+            'quartz' => 0,
+            'naqahdah' => 0,
+            'production_type' => 'gold',
+            'production_coefficient' => 1.8,
+            'energy_base' => NULL,
+            'energy_coefficient' => NULL,
+            'display_order' => 2,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1800,
+            'time_coefficient' => 1.5
+        ]);
+
+        DB::table('buildings')->insert([
+            'id' => 13,
+            'name' => 'Entrepôt de quartz',
+            'description' => "Permet d'augmenter la capacité de stockage en Quartz de 1.8 / LVL",
+            'type' => 'Storage',
+            'iron' => 2000,
+            'gold' => 2000,
+            'quartz' => 0,
+            'naqahdah' => 0,
+            'production_type' => 'quartz',
+            'production_coefficient' => 1.8,
+            'energy_base' => NULL,
+            'energy_coefficient' => NULL,
+            'display_order' => 3,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1800,
+            'time_coefficient' => 1.5
+        ]);
+
+        DB::table('buildings')->insert([
+            'id' => 14,
+            'name' => 'Entrepôt de naqahdah',
+            'description' => "Permet d'augmenter la capacité de stockage en Naqahdah de 1.8 / LVL",
+            'type' => 'Storage',
+            'iron' => 2000,
+            'gold' => 1000,
+            'quartz' => 1000,
+            'naqahdah' => 0,
+            'production_type' => 'naqahdah',
+            'production_coefficient' => 1.8,
+            'energy_base' => NULL,
+            'energy_coefficient' => NULL,
+            'display_order' => 4,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1800,
+            'time_coefficient' => 1.5
+        ]);
         
+        DB::table('technologies')->insert([
+            'id' => 1,
+            'name' => 'Informatique et Communication',
+            'description' => "Doté d'un système informatique et de communication performants, vos colons sont plus efficaces dans leurs tâches.\n__Bonus__\n-5% Temps de construction\n-5% Temps de recherche",
+            'type' => 'Labo',
+            'iron' => 0,
+            'gold' => 400,
+            'quartz' => 600,
+            'naqahdah' => 0,
+            'display_order' => 1,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1602,
+            'time_coefficient' => 2
+        ]);
+
+        DB::table('technologies')->insert([
+            'id' => 2,
+            'name' => 'Espionnage',
+            'description' => "Détermine votre efficacité lors de l'espionnage d'une planète ennemie",
+            'type' => 'Labo',
+            'iron' => 100,
+            'gold' => 1000,
+            'quartz' => 200,
+            'naqahdah' => 0,
+            'display_order' => 2,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1500,
+            'time_coefficient' => 2
+        ]);
+
+        DB::table('technologies')->insert([
+            'id' => 3,
+            'name' => 'Contre-Espionage',
+            'description' => "Détermine votre efficacité à contre la tentative d'espionage d'un joueur étranger",
+            'type' => 'Labo',
+            'iron' => 200,
+            'gold' => 2000,
+            'quartz' => 400,
+            'naqahdah' => 0,
+            'display_order' => 3,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1500,
+            'time_coefficient' => 2
+        ]);
+
+        DB::table('technologies')->insert([
+            'id' => 3,
+            'name' => 'Energie',
+            'description' => "Permet de maîtriser d'avantage l'énergie, vous octroyant un bonus de 5% du rendement des bâtiments d'énergie",
+            'type' => 'Labo',
+            'iron' => 200,
+            'gold' => 2000,
+            'quartz' => 400,
+            'naqahdah' => 0,
+            'display_order' => 3,
+            'upgrade_coefficient' => 2,
+            'time_base' => 1500,
+            'time_coefficient' => 2
+        ]);
         /*
         DB::table('players')->insert([
             'id' => 1,
