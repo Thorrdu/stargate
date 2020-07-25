32575ec29c8da5e95291f92e4b62db9f0623787f
Thorrdu <thierry.hansens@gmail.com>
2020-07-26 01:52:54 +0200

    fix

--- laravel/database/migrations/2020_07_23_215422_technology_buildings.php
+++ laravel/database/migrations/2020_07_23_215422_technology_buildings.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TechnologyBuilding extends Migration
+class TechnologyBuildings extends Migration
 {
     /**
      * Run the migrations.
