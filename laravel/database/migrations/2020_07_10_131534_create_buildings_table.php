110ddcaf1a67fb836aed24bf499e7b956184756d
Thorrdu <thierry.hansens@gmail.com>
2020-07-28 13:03:12 +0200

    slug

--- laravel/database/migrations/2020_07_10_131534_create_buildings_table.php
+++ laravel/database/migrations/2020_07_10_131534_create_buildings_table.php
@@ -16,6 +16,7 @@ class CreateBuildingsTable extends Migration
         Schema::create('buildings', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 50);
+            $table->string('slug', 50);
             $table->longText('description');
 
             $table->enum('type', ['Energy', 'Production', 'Storage', 'Science', 'Military']);
--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 1,
             'name' => 'Centrale Thermique',
+            'slug' => Str::slug('thermalreactor'),
             'description' => 'Reacteur permettant d\'exploiter l\'énergie thermique souteraine présente sur votre colonie',
             'type' => 'Energy',
             'iron' => 52,
@@ -43,6 +44,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 2,
             'name' => 'Mine de fer',
+            'slug' => Str::slug('ironmine'),
             'description' => 'Mine rudimentaire permettant d\'extraire du minerais de fer',
             'type' => 'Production',
             'iron' => 60,
@@ -63,6 +65,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 3,
             'name' => 'Mine d\'or',
+            'slug' => Str::slug('goldmine'),
             'description' => 'Mine rudimentaire permettant d\'extraire de l\'or',
             'type' => 'Production',
             'iron' => 50,
@@ -83,6 +86,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 4,
             'name' => 'Mine de quartz',
+            'slug' => Str::slug('quartzmine'),
             'description' => 'Mine rudimentaire permettant d\'extraire de quartz',
             'type' => 'Production',
             'iron' => 500,
@@ -102,6 +106,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 5,
             'name' => 'Extracteur de naqahdah',
+            'slug' => Str::slug('naqahdahmine'),
             'description' => 'Extracteur de naqahdah',
             'type' => 'Production',
             'iron' => 500,
@@ -121,6 +126,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 6,
             'name' => 'Usine robotisée',
+            'slug' => Str::slug('robotfactory'),
             'description' => "Permet à vos colons de travailler avec le support de robots.\nRéduit le temps de construction des bâtiments, vaissaux, défenses et objets de 10% par niveau",
             'type' => 'Science',
             'iron' => 100,
@@ -142,6 +148,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 7,
             'name' => 'Centre de recherche',
+            'slug' => Str::slug('research'),
             'description' => "Donne un lieu à vos colons pour effectuer des recherches, réduit le temps des recherches de 10% par niveau.\nPermet également de comprendre le fonctionnement de la porte des étoiles",
             'type' => 'Science',
             'iron' => 200,
@@ -163,6 +170,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 8,
             'name' => 'Station de clônage',
+            'slug' => Str::slug('clone'),
             'description' => "Obtenue de part l'alliance avec les Asgards, la station de clônage permet de recruter de nouvelles unités",
             'type' => 'Military',
             'iron' => 500,
@@ -189,6 +197,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 9,
             'name' => 'Chantier Spacial',
+            'slug' => Str::slug('spacial'),
             'description' => "Permet de développer la construction de sondes et vaisseaux spaciaux",
             'type' => 'Military',
             'iron' => 100,
@@ -212,6 +221,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 10,
             'name' => 'Reacteur au Naqahdah',
+            'slug' => Str::slug('naqadahreactor'),
             'description' => "Centrale permettant de générer d'énormes quantités d'énergie en consommant du Naqahdah.",
             'type' => 'Energy',
             'iron' => 5200,
@@ -231,6 +241,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 11,
             'name' => 'Entrepôt de fer',
+            'slug' => Str::slug('ironstorage'),
             'description' => "Permet de multiplier la capacité de stockage en Fer par 1.8 / LVL",
             'type' => 'Storage',
             'iron' => 2000,
@@ -256,6 +267,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 12,
             'name' => 'Entrepôt d\'Or',
+            'slug' => Str::slug('goldstorage'),
             'description' => "Permet de multiplier la capacité de stockage en Or par 1.8 / LVL",
             'type' => 'Storage',
             'iron' => 2000,
@@ -281,6 +293,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 13,
             'name' => 'Entrepôt de quartz',
+            'slug' => Str::slug('quartzstorage'),
             'description' => "Permet de multiplier la capacité de stockage en Quartz par 1.8 / LVL",
             'type' => 'Storage',
             'iron' => 2000,
@@ -306,6 +319,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 14,
             'name' => 'Entrepôt de naqahdah',
+            'slug' => Str::slug('naqahdahstorage'),
             'description' => "Permet de multiplier la capacité de stockage en Naqahdah par 1.8 / LVL",
             'type' => 'Storage',
             'iron' => 2000,
@@ -331,6 +345,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 15,
             'name' => 'Centre de défense',
+            'slug' => Str::slug('defense'),
             'description' => "Permet à votre colonie de se défendre en cas d'attaques.",
             'type' => 'Military',
             'iron' => 75,
@@ -354,6 +369,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 1,
             'name' => 'Informatique et Communication',
+            'slug' => Str::slug('communication'),
             'description' => "Doté d'un système informatique et de communication performants, vos colons sont plus efficaces dans leurs tâches.\n__Bonus__\n-5% Temps de construction\n-5% Temps de recherche",
             'type' => 'Labo',
             'iron' => 0,
@@ -377,6 +393,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 2,
             'name' => 'Espionnage',
+            'slug' => Str::slug('spy'),
             'description' => "Détermine votre efficacité lors de l'espionnage d'une planète ennemie",
             'type' => 'Labo',
             'iron' => 100,
@@ -398,6 +415,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 3,
             'name' => 'Contre-Espionage',
+            'slug' => Str::slug('counterspy'),
             'description' => "Détermine votre efficacité à contre la tentative d'espionage d'un joueur étranger",
             'type' => 'Labo',
             'iron' => 200,
@@ -425,6 +443,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 4,
             'name' => 'Energie',
+            'slug' => Str::slug('energy'),
             'description' => "Permet de maîtriser d'avantage l'énergie, vous octroyant un bonus de 5% du rendement des bâtiments d'énergie par niveau",
             'type' => 'Labo',
             'iron' => 0,
@@ -453,6 +472,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 5, // -2% building -2% techno
             'name' => 'Intelligence artificielle', //nanotechnologie
+            'slug' => Str::slug('ia'),
             'description' => "Permet de développer une intelligence artificielle capable de vous aider au quotidien sur votre colonie\nBonus: -2% Temps de construction / recherche",
             'type' => 'Labo',
             'iron' => 4000,
@@ -483,6 +503,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 6, 
             'name' => 'Plans de vaisseaux',
+            'slug' => Str::slug('blueprint'),
             'description' => "Permet la conception de vaisseaux spaciaux.",
             'type' => 'Center',
             'iron' => 550,
@@ -504,6 +525,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 7, 
             'name' => 'Armement',
+            'slug' => Str::slug('Armament'),
             'description' => "Permet la conception d'armes.",
             'type' => 'Center',
             'iron' => 400,
@@ -525,6 +547,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 8, 
             'name' => 'Coque',
+            'slug' => Str::slug('hull'),
             'description' => "Permet la conception de coques de vaisseaux.",
             'type' => 'Center',
             'iron' => 800,
@@ -554,6 +577,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 9, 
             'name' => 'Bouclier',
+            'slug' => Str::slug('shield'),
             'description' => "Permet la conception de boucliers.",
             'type' => 'Center',
             'iron' => 400,
@@ -581,6 +605,7 @@ class DatabaseSeeder extends Seeder
         DB::table('technologies')->insert([
             'id' => 10, 
             'name' => 'Combustion',
+            'slug' => Str::slug('afterburner'),
             'description' => "Permet la conception de moteurs conventionels.",
             'type' => 'Center',
             'iron' => 2400,
@@ -614,6 +639,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 16,
             'name' => 'Centre de commandement', //département de nanorobotique
+            'slug' => Str::slug('commandcenter'),
             'description' => "Centre de commandement équipé d'une intelligence articifielle hors du commun. Votre vie sur cette colonie sera désormais bien plus aisée.".
                              "Bonus: -50% Temps de construction / recherche", //et moins 30% les vaisseaux
             'type' => 'Military',
@@ -651,6 +677,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 17,
             'name' => 'Mine de fer avancée',
+            'slug' => Str::slug('ironadvancedmine'),
             'description' => "Désormais habitués à miner du fer sur cette colonie, vos volons ont dévellopés une manière bien plus éfficace d'extraire le fer de la planete",
             'type' => 'Production',
             'iron' => 30000,
@@ -675,6 +702,7 @@ class DatabaseSeeder extends Seeder
         DB::table('buildings')->insert([
             'id' => 18,
             'name' => 'Mine d\'or avancée',
+            'slug' => Str::slug('goldadvancedmine'),
             'description' => "Désormais habitués à miner de l\'or sur cette colonie, vos volons ont dévellopés une manière bien plus éfficace d'extraire l\'or de la planete",
             'type' => 'Production',
             'iron' => 32000,
