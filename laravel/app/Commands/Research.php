d7169f03dc0f4189983ed8039682edf896c4a4c9
Thorrdu <thierry.hansens@gmail.com>
2020-07-28 21:16:03 +0200

    Recherche rework

--- laravel/app/Commands/Research.php
+++ laravel/app/Commands/Research.php
@@ -68,59 +68,149 @@ class Research extends CommandHandler implements CommandInterface
                 }
                 else
                 {
-                    $technologyId = (int)$this->args[0];
-                    $technology = Technology::find($technologyId);
+                    $technology = Technology::where('id', (int)$this->args[0])->orWhere('slug', 'LIKE', $this->args[0].'%')->first();
                     if(!is_null($technology))
                     {
-                        //if recherche en cours, return
-                        if(!is_null($this->player->active_technology_end))
-                            return 'Une technologie est déjà en cours de recherche';
-
-                        $wantedLvl = 1;
-                        $currentLevel = $this->player->hasTechnology($technology);
-                        if($currentLevel)
-                            $wantedLvl += $currentLevel;
-
-                        $hasEnough = true;
-                        $technologyPrices = $technology->getPrice($wantedLvl);
-                        foreach (config('stargate.resources') as $resource)
+                        if(count($this->args) == 2 && in_array($this->args[1],array('conf','confirm')))
                         {
-                            if($technology->$resource > 0 && $technologyPrices[$resource] > $this->player->colonies[0]->$resource)
-                                $hasEnough = false;
+                            //Requirement
+                            $hasRequirements = true;
+                            foreach($technology->requiredTechnologies as $requiredTechnology)
+                            {
+                                $currentLvl = $this->player->hasTechnology($requiredTechnology);
+                                if(!($currentLvl && $currentLvl >= $requiredTechnology->pivot->level))
+                                    $hasRequirements = false;
+                            }
+                            foreach($technology->requiredBuildings as $requiredBuilding)
+                            {
+                                $currentLvl = $this->player->colonies[0]->hasBuilding($requiredBuilding);
+                                if(!($currentLvl && $currentLvl >= $requiredBuilding->pivot->level))
+                                    $hasRequirements = false;
+                            }
+
+                            if(!$hasRequirements)
+                                return 'Vous ne possédez pas assez les pré-requis pour cette recherche.';
+
+                            //if recherche en cours, return
+                            if(!is_null($this->player->active_technology_end))
+                                return 'Une technologie est déjà en cours de recherche';
+
+                            $wantedLvl = 1;
+                            $currentLevel = $this->player->hasTechnology($technology);
+                            if($currentLevel)
+                                $wantedLvl += $currentLevel;
+
+                            $hasEnough = true;
+                            $technologyPrices = $technology->getPrice($wantedLvl);
+                            foreach (config('stargate.resources') as $resource)
+                            {
+                                if($technology->$resource > 0 && $technologyPrices[$resource] > $this->player->colonies[0]->$resource)
+                                    $hasEnough = false;
+                            }
+
+                            if(!$hasEnough)
+                                return 'Vous ne possédez pas assez de ressource pour rechercher cette technologie.';
+
+                            if( !is_null($this->player->colonies[0]->active_building_id) && $this->player->colonies[0]->active_building_id == 7)
+                                return 'Votre centre de recherche est occupé...';
+
+                            $endingDate = Carbon::createFromFormat("Y-m-d H:i:s",$this->player->startResearch($technology))->timestamp;
+                            $buildingTime = gmdate("H:i:s", $endingDate - time());
+
+                            return 'Recherche commencée, **'.$technology->name.' LVL '.$wantedLvl.'** sera terminé dans '.$buildingTime;
                         }
-
-                        if(!$hasEnough)
-                            return 'Vous ne possédez pas assez de ressource pour rechercher cette technologie.';
-
-                        if( !is_null($this->player->colonies[0]->active_building_id) && $this->player->colonies[0]->active_building_id == 7)
-                            return 'Votre centre de recherche est occupé...';
-
-                        //Requirement
-                        $hasRequirements = true;
-                        foreach($technology->requiredTechnologies as $requiredTechnology)
+                        else
                         {
-                            $currentLvl = $this->player->hasTechnology($requiredTechnology);
-                            if(!($currentLvl && $currentLvl >= $requiredTechnology->pivot->level))
-                                $hasRequirements = false;
+                            $hasRequirements = true;
+                            foreach($technology->requiredTechnologies as $requiredTechnology)
+                            {
+                                $currentLvlOwned = $this->player->hasTechnology($requiredTechnology);
+                                if(!($currentLvlOwned && $currentLvlOwned >= $requiredTechnology->pivot->level))
+                                    $hasRequirements = false;
+                            }
+                            foreach($technology->requiredBuildings as $requiredBuilding)
+                            {
+                                $currentLvlOwned = $this->player->colonies[0]->hasBuilding($requiredBuilding);
+                                if(!($currentLvlOwned && $currentLvlOwned >= $requiredBuilding->pivot->level))
+                                    $hasRequirements = false;
+                            }
+
+                            if(!$hasRequirements)
+                            {
+                                return "Vous n'avez pas encore découvert cette technologie.";
+                            }
+
+                            $wantedLevel = 1;
+                            $currentLvl = $this->player->hasTechnology($technology);
+                            if($currentLvl)
+                                $wantedLevel += $currentLvl;
+                
+                            $buildingPrice = "";
+                            $buildingPrices = $technology->getPrice($wantedLevel);
+                            foreach (config('stargate.resources') as $resource)
+                            {
+                                if($technology->$resource > 0)
+                                {
+                                    $buildingPrice .= number_format(round($buildingPrices[$resource])).' '.ucfirst($resource)."\n";
+                                }
+                            }
+                
+                            $buildingTime = $technology->getTime($wantedLevel);
+                            
+                            /** Application des bonus */
+                            $buildingTime *= $this->player->colonies[0]->getResearchBonus();
+                
+                            $buildingTime = gmdate("H:i:s", $buildingTime);
+                
+                            $displayedLvl = 0;
+                            if($currentLvl)
+                                $displayedLvl = $currentLvl;
+
+                            $bonusString = "";
+                            if(!is_null($technology->energy_bonus))
+                                $bonusString += "Energie produite: +{($technology->energy_bonus*100)}%\n";
+                            if(!is_null($technology->building_bonus))
+                                $bonusString += "Temps de construction des bâtiments: -{($technology->building_bonus*100)}%\n";
+                            if(!is_null($technology->technology_bonus))
+                                $bonusString += "Temps de recherche: -{($technology->technology_bonus*100)}%\n";
+
+                            $embed = [
+                                'author' => [
+                                    'name' => $this->player->user_name,
+                                    'icon_url' => 'https://cdn.discordapp.com/avatars/730815388400615455/267e7aa294e04be5fba9a70c4e89e292.png'
+                                ],
+                                "title" => $technology->name.' - LVL '.$displayedLvl,
+                                "description" => $technology->description,
+                                'fields' => [
+                                    [
+                                        'name' => "Info",
+                                        'value' => "ID: ".$technology->id."\n"."Slug: ".$technology->slug,
+                                        'inline' => true
+                                    ],
+                                    [
+                                        'name' => "Bonus",
+                                        'value' => $bonusString,
+                                        'inline' => true
+                                    ],
+                                    [
+                                        'name' => "Prix",
+                                        'value' => $buildingPrice,
+                                        'inline' => true
+                                    ],
+                                    [
+                                        'name' => "Durée de recherche",
+                                        'value' => $buildingTime,
+                                        'inline' => true
+                                    ]
+                                ],
+                                'footer' => array(
+                                    'text'  => 'Stargate',
+                                ),
+                            ];
                         }
-                        foreach($technology->requiredBuildings as $requiredBuilding)
-                        {
-                            $currentLvl = $this->player->colonies[0]->hasBuilding($requiredBuilding);
-                            if(!($currentLvl && $currentLvl >= $requiredBuilding->pivot->level))
-                                $hasRequirements = false;
-                        }
-
-                        if(!$hasRequirements)
-                            return 'Vous ne possédez pas assez les pré-requis pour cette recherche.';
-
-
-
-                        $endingDate = Carbon::createFromFormat("Y-m-d H:i:s",$this->player->startResearch($technology))->timestamp;
-                        $buildingTime = gmdate("H:i:s", $endingDate - time());
-
-                        return 'Recherche commencée, **'.$technology->name.' LVL '.$wantedLvl.'** sera terminé dans '.$buildingTime;
-
                     }
+                    else
+                        return 'Technologie inconnue...';
                 }
 
             }
@@ -204,8 +294,8 @@ class Research extends CommandHandler implements CommandInterface
                     $conditionsValue .= " / ";
                 $conditionsValue .= $requiredBuilding->name.' - LVL '.$requiredBuilding->pivot->level;
             }
-            if(!empty($conditionsValue))
-                $conditionsValue = "\nCondition: ".$conditionsValue;
+            /*if(!empty($conditionsValue))
+                $conditionsValue = "\nCondition: ".$conditionsValue;*/
 
             if($hasRequirements == true)
             {
