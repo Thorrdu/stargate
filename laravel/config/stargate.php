7c9df5b8c5a92383219ea76e69bbb28635221814
Thorrdu <thierry.hansens@gmail.com>
2020-07-29 20:09:40 +0200

    emote refresh

--- laravel/config/stargate.php
+++ laravel/config/stargate.php
@@ -16,7 +16,14 @@ return [
         'iron' => '<:iron:737769180190998680>',
         'gold' => '<:gold:737769237011234917>',
         'quartz' => '<:quartz:737769265104551987>',
-        'naqahdah' => '<:naqahdah:737769280573276160>'
-
+        'naqahdah' => '<:naqahdah:737769280573276160>',
+        'clone' => '<:clone:737772878723940455>',
+        'e2pz' => '<:e2pz:737772133559697478>',
+        'energy' => '<:energy:737769505790492782>',
+        'production' => '<:production:737769455915892757>',
+        'productionBuilding' => '⚙️',
+        'research' => '<:research:737769578045898775>',
+        'military' => '<:military:737769413629182094>',
+        'storage' => '<:storage:737773247336022077>'
     ]
 ];
