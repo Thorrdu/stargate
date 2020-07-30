cef0e3d1360c832bca6226c3a9737d6a3abe51f2
Thorrdu <thierry.hansens@gmail.com>
2020-07-31 01:28:57 +0200

    fix ping

--- laravel/app/Commands/Ping.php
+++ laravel/app/Commands/Ping.php
@@ -12,7 +12,7 @@ class Ping extends CommandHandler implements CommandInterface
     {
         echo PHP_EOL.'Execute Ping';
         $this->message->channel->sendMessage('Ping...')->then(function ($messageSent){
-            $latency = $messageSent->timestamp - $this->message->timestamp;
+            $latency = ($messageSent->timestamp->timestamp.$messageSent->timestamp->milli) - ($this->message->timestamp->timestamp.$this->message->timestamp->milli);
             $messageSent->channel->editMessage($messageSent->id, 'Pong! Latency: '.$latency.'ms');
         });
     }
