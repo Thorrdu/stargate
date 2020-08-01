206ca85d3b6d913882ab3bd35d385b6ee868fe80
Thorrdu <thierry.hansens@gmail.com>
2020-08-02 00:23:18 +0200

    trad en

--- laravel/resources/lang/en/start.php
+++ laravel/resources/lang/en/start.php
@@ -3,14 +3,14 @@
 return [
     "accountExists" => "You already own an account...",
     "langChoice" => "Welcome to Stargate,\n\nBefore you go to your new adventure, please clic on the reaction that correspond to your language.",
-    "startMessage" => "Et voici comment tout à commencé,\n\n"
-                      ."Ravis d'avoir enfin été choisi pour participer au programme SGC et affecté à la nouvelle base de la terre sur une planète lointaine.\n"
-                      ."Ce que personne n'avait prévu, c'est qu'au moment où vous traverseriez pour rejoindre votre nouvelle affectaction, c'est qu'une anomalie se trouverait sur la trajectoire du vortex vous reliant à la dite planète.\n"
-                      ."\nRésultat, vous vous retrouvez seul sur un monde inconnu donc la disposition des étoiles dans le ciel n'ont rien de commun avec celles de votre bonne vieille Voie lactée.\n"
-                      ."Avec vous se trouve une dizaine d'hommes et de femmes, quelques caisses de matériel et pour vue, une porte des étoiles en piteux état ,sans DHD apparent.\n"
-                      ."Vous n'avez pas tardé à être nommé chef de ce groupe de malchanceux et avez exploré la planète sur laquelle vous vous trouvez et avez choisi un emplacement pour vous installer.\n"
-                      ."En tant que leader de cette nouvelle colonie, votre rôle est de développer et de défendre votre colonie, explorer la galaxie et trouver un moyen de rentrer sur terre.\n"
-                      ."\nPour consulter les informations de votre colonie, utilisez `!colony` (ou `!c`) \n"
-                      ."\n\n/!\ Warning /!\ Bot en Early Alpha, la majorité des mécaniques de jeu n'ont pas encore été implémentées et de nombreux changements sont suceptibles de survenir ainsi que d'éventuels reset!" 
-                      ."\nMerci de rapporter tout bug sur le serveur de support" 
+    "startMessage" => "This is how everything started,\n\n"
+                      ."Glad to finally been chosen to join the SGC program and affected to the new base on an away planet.\n"
+                      ."What that nobody could knew is that at the time you'll cross the Stargate to join your new job, a problem will appear on the way linking to the planet.\n"
+                      ."\nSo you're now alone on an unknown place and stars are non common with your good old Milky Way.\n"
+                      ."With you there is only a douzen men and women, a few supply crates and in sight just a shattered Stargate without any DHD. \n"
+                      ."With not delay you've been chosen as the new leader of this unlucky group and you decided to explore this planet to finally found a new place to settle.\n"
+                      ."As a leader on this new settlement, your role is to develop and defend your place, explore the galaxy and find a way to go back on Earth.\n"
+                      ."\nTo consult informations on your settlement, use `!colony` (or `!c`) \n"
+                      ."\n\n/!\ Warning /!\ Bot is in Early Alpha, mist of mechanics didn't been implement and a lot of changements could appear as some reset!" 
+                      ."\n Thanks to report any problems on the support server" 
 ];
