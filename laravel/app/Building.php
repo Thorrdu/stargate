01e8a6b0f569347e9de9efbeaf93ffe04b04dc6d
Thorrdu <thierry.hansens@gmail.com>
2020-08-01 22:17:08 +0200

    fix

--- laravel/app/Building.php
+++ laravel/app/Building.php
@@ -39,7 +39,7 @@ class Building extends Model
     {
         //$level--; //Du au coeficient
         //return floor($this->energy_base * pow($this->energy_coefficient, $level));
-        return $this->coefCalc($this->energy_base,$this->energy_coefficient,$level);
+        return floor($this->coefCalc($this->energy_base,$this->energy_coefficient,$level));
 
     }
 
