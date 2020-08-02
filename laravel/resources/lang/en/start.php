3296ef0b380b1b1d8bca523c5ee55a8061251cb7
Thorrdu <thierry.hansens@gmail.com>
2020-08-02 16:30:46 +0200

    fix start lang

--- laravel/resources/lang/en/start.php
+++ laravel/resources/lang/en/start.php
@@ -4,13 +4,13 @@ return [
     "accountExists" => "You already own an account...",
     "langChoice" => "Welcome to Stargate,\n\nBefore you go to your new adventure, please clic on the reaction that correspond to your language.",
     "startMessage" => "This is how everything started,\n\n"
-                      ."Glad to finally been chosen to join the SGC program and affected to the new base on an away planet.\n"
-                      ."What that nobody could knew is that at the time you'll cross the Stargate to join your new job, a problem will appear on the way linking to the planet.\n"
+                      ."Thrilled to finally been chosen to join the SGC program and affected to Earth's new base on an distant planet.\n"
+                      ."What no one anticipated was that at the time you crossed the Stargate to join your new assignement, an anomaly would appear on the way to the planet.\n"
                       ."\nSo you're now alone on an unknown place and stars are non common with your good old Milky Way.\n"
                       ."With you there is only a douzen men and women, a few supply crates and in sight just a shattered Stargate without any DHD. \n"
-                      ."With not delay you've been chosen as the new leader of this unlucky group and you decided to explore this planet to finally found a new place to settle.\n"
-                      ."As a leader on this new settlement, your role is to develop and defend your place, explore the galaxy and find a way to go back on Earth.\n"
-                      ."\nTo consult informations on your settlement, use `!colony` (or `!c`) \n"
-                      ."\n\n/!\ Warning /!\ Bot is in Early Alpha, mist of mechanics didn't been implement and a lot of changements could appear as some reset!" 
-                      ."\n Thanks to report any problems on the support server" 
+                      ."Yopu were quickly named as the new leader of this unlucky group and you decided to explore this planet to finally found a new place to settle.\n"
+                      ."As a leader on this new colony, your role is to develop and defend your place, explore the galaxy and find a way to go back on Earth.\n"
+                      ."\nTo check your colony's information, use `!colony` (or `!c`) \n"
+                      ."\n\n/!\ Warning /!\ This Bot is in Early Alpha, the majority of the game mechanics have not yet been implemented and many changes are likely to occur, including possible resets!" 
+                      ."\nThanks to report any problems on the support server" 
 ];
