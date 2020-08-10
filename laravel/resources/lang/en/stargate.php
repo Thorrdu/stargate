090fbfd25f9c5fd0713047fd31d0381513403eb7
Thorrdu <thierry.hansens@gmail.com>
2020-08-10 03:52:22 +0200

    stargate trad

--- laravel/resources/lang/en/stargate.php
+++ laravel/resources/lang/en/stargate.php
@@ -9,4 +9,15 @@ return [
     "unknownCoordinates" => "Unknown Coordinates",
     "stargateShattered" => "The Stargate is shattered and not ready for proper usage.\nMay be giving your scientist more resources could help solve this problem...",
     "failedDialing" => "The dialing to this planet failed. There is no gate to contact on those coordinates.",
+    "exploreSucessResources" => "En explorant la planète [:coordinates], l'équipe est tombée sur un entrepôt contenant des ressources intéréssantes.\n".
+    "Ils ont ramenés avec eux: :resources",
+    "explorePlayerImpossible" => "Il n'est pas possible d'explorer la planète d'un autre joueur. essayez l'espionnage.",
+    "exploreFailed" => "Vos scientifiques n'ont rien trouvé d'intéréssant en explorant la planète [:coordinates].",
+    "exploreSucessBuildingTip" => "Vos scientifiques ont trouvé l'information suivante en explorant la planète [:coordinates]:\n".
+        "Le bâtiment :name requiert :lvlRequirement: :nameRequirement",
+    "exploreSucessTechnologyTip" => "Vos scientifiques ont trouvé l'information suivante en explorant la planète [:coordinates]:\n".
+        "La technologie :name requiert :lvlRequirement: :nameRequirement",
+    "exploreSucessCraftTip" => "Vos scientifiques ont trouvé l'information suivante en explorant la planète [:coordinates]:\n".
+    "Le craft :name requiert :lvlRequirement: :nameRequirement",
+    "exploreCriticalFailed" => "L'équipe envoyée sur [:coordinates] n'a envoyé aucun signe de vie",
 ];
--- laravel/resources/lang/fr/stargate.php
+++ laravel/resources/lang/fr/stargate.php
@@ -9,4 +9,15 @@ return [
     "unknownCoordinates" => "Coordonées inconnues",
     "stargateShattered" => "La porte des étoiles est actuellement hors service.\nFournir d'avantage de ressources à vos scientifiques pourrait être la solution...",
     "failedDialing" => "L'appel vers cette planète à échoué. Il n'y a pas de porte à contacter à ces coordonées.",
+    "exploreSucessResources" => "En explorant la planète [:coordinates], l'équipe est tombée sur un entrepôt contenant des ressources intéréssantes.\n".
+                                "Ils ont ramenés avec eux: :resources",
+    "explorePlayerImpossible" => "Il n'est pas possible d'explorer la planète d'un autre joueur. essayez l'espionnage.",
+    "exploreFailed" => "Vos scientifiques n'ont rien trouvé d'intéréssant en explorant la planète [:coordinates].",
+    "exploreSucessBuildingTip" => "Vos scientifiques ont trouvé l'information suivante en explorant la planète [:coordinates]:\n".
+                                  "Le bâtiment :name requiert :lvlRequirement: :nameRequirement",
+    "exploreSucessTechnologyTip" => "Vos scientifiques ont trouvé l'information suivante en explorant la planète [:coordinates]:\n".
+                                  "La technologie :name requiert :lvlRequirement: :nameRequirement",
+    "exploreSucessCraftTip" => "Vos scientifiques ont trouvé l'information suivante en explorant la planète [:coordinates]:\n".
+                               "Le craft :name requiert :lvlRequirement: :nameRequirement",
+    "exploreCriticalFailed" => "L'équipe envoyée sur [:coordinates] n'a envoyé aucun signe de vie",
 ];
