a86572a4a6e1c79bf78c291c21be06155b4e5bd2
Thorrdu <thierry.hansens@gmail.com>
2020-08-10 02:08:21 +0200

    fxi trad

--- laravel/resources/lang/fr/stargate.php
+++ laravel/resources/lang/fr/stargate.php
@@ -8,5 +8,5 @@ return [
                           "attack\n",
     "unknownCoordinates" => "Coordonées inconnues",
     "stargateShattered" => "La porte des étoiles est actuellement hors service.\nFournir d'avantage de ressources à vos scientifiques pourrait être la solution...",
-    "failedDialing" => "L'appel à cette planète à échoué. Il n'y a pas de porte à contacter à ces coordonées.",
+    "failedDialing" => "L'appel vers cette planète à échoué. Il n'y a pas de porte à contacter à ces coordonées.",
 ];
