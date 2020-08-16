7376f5faa6a99fa21012fdf3ff572a26f65b664e
Thorrdu <thierry.hansens@gmail.com>
2020-08-16 14:59:32 +0200

    building translation

--- laravel/resources/lang/en/building.php
+++ laravel/resources/lang/en/building.php
@@ -1,6 +1,83 @@
 <?php
 //BUILDING EN
 return [
+    'thermalreactor' => [
+        'name' => 'Thermal reactor',
+        'description' => 'Reactor to exploit the underground thermal energy of your colony'
+    ],
+    'ironmine' => [
+        'name' => 'Iron mine',
+        'description' => 'Basic mine to extract Iron ore'
+    ],
+    'goldmine' => [
+        'name' => 'Gold mine',
+        'description' => 'Basic mine to extract Gold ore'
+    ],
+    'quartzmine' => [
+        'name' => 'Quartz mine',
+        'description' => 'Basic mine to extract Quartz'
+    ],
+    'naqahdahmine' => [
+        'name' => 'Naqahdah extractor',
+        'description' => 'Extractor alowing to recover Naqadah from lower layers of the planet'
+    ],
+    'robotfactory' => [
+        'name' => 'Robot factory',
+        'description' => 'Allow your settlers to be helped by robots when working'
+    ],
+    'research' => [
+        'name' => 'Research center',
+        'description' => "Give a location  to your settlers to make research, reduce the research time by 10% per level.\n".
+                         "Moreover it allows to understand the exploitation of the Stargate"
+    ],
+    'military' => [
+        'name' => 'Recruitement center',
+        'description' => 'You can recruit natives from your new planet and make them militaries able to help you in fights or exploring new worlds.'
+    ],
+    'shipyard' => [
+        'name' => 'Shipyard',
+        'description' => 'Allows to develop the building of probes and space ships'
+    ],
+    'naqadahreactor' => [
+        'name' => 'Naqahdah reactor',
+        'description' => 'Reactor that allows to generate huge amount of energy but consume some Naqahdah'
+    ],
+    'ironstorage' => [
+        'name' => 'Iron storage',
+        'description' => 'Multiply your Iron storage capacity by 1.8/lvl'
+    ],
+    'goldstorage' => [
+        'name' => 'Gold storage',
+        'description' => 'Multiply your Gold storage capacity by 1.8/lvl'
+    ],
+    'quartzstorage' => [
+        'name' => 'Quartz storage',
+        'description' => 'Multiply your Quartz storage capacity by 1.8/lvl'
+    ],
+    'naqahdahstorage' => [
+        'name' => 'Naqadah storage',
+        'description' => 'Multiply your Naqahdah storage capacity by 1.8/lvl'
+    ],
+    'defence' => [
+        'name' => 'Defence center',
+        'description' => 'Allows to defend your settlement under attacks'
+    ],
+    'commandcenter' => [
+        'name' => 'Command center',
+        'description' => 'Command Center equipped with an out of common Artificial Intelligence. It will increase your life quality on this colony.'
+    ],
+    'ironadvancedmine' => [
+        'name' => 'Advanced Iron mine',
+        'description' => 'Now used to mining iron on this colony, your settlers developed a much better way to extract it on this planet.'
+    ],
+    'goldadvancedmine' => [
+        'name' => 'Advanced Gold mine',
+        'description' => 'Now used to mining gold on this colony, your settlers developed a much better way to extract it on this planet.'
+    ],
+    'asuranfactory' => [
+        'name' => 'Asuran factory',
+        'description' => "After long analysis, you scientists have succed to activate an old Asuran factory of ZPM. It opens you the way to intergalactis travels through the stargate"
+    ],
     'hiddenBuilding' => '-- Hidden building --',
     'unDiscovered' => 'Undiscovered',
     'unknownBuilding' => 'Unknown building...',
--- laravel/resources/lang/fr/building.php
+++ laravel/resources/lang/fr/building.php
@@ -1,6 +1,83 @@
 <?php
 //BUILDING FR
 return [
+    'thermalreactor' => [
+        'name' => 'Centrale Thermique',
+        'description' => "Reacteur permettant d'exploiter l'énergie thermique souteraine présente sur votre colonie"
+    ],
+    'ironmine' => [
+        'name' => 'Iron mine',
+        'description' => "Mine rudimentaire permettant d'extraire du minerais de fer"
+    ],
+    'goldmine' => [
+        'name' => "Mine d'or",
+        'description' => "Mine rudimentaire permettant d'extraire de l'or"
+    ],
+    'quartzmine' => [
+        'name' => 'Mine de quartz',
+        'description' => "Mine rudimentaire permettant d'extraire de quartz"
+    ],
+    'naqahdahmine' => [
+        'name' => 'Extracteur de naqahdah',
+        'description' => 'Extracteur permettant de récupérer du Naqahdah dans les couches inférieures de la planète'
+    ],
+    'robotfactory' => [
+        'name' => 'Usine robotisée',
+        'description' => 'Permet à vos colons de travailler avec le support ...'
+    ],
+    'research' => [
+        'name' => 'Centre de recherche',
+        'description' => "Donne un lieu à vos colons pour effectuer des recherches.".
+                         "\nPermet également de comprendre le fonctionnement de la porte des étoiles"
+    ],
+    'military' => [
+        'name' => 'Centre de recrutement',
+        'description' => "Vous permet de recruter les autochtones de votre nouvelle planète et en faire des militaires capables de vous aider au combat ou en exploration."
+    ],
+    'shipyard' => [
+        'name' => 'Chantier Spatial',
+        'description' => 'Permet de développer la construction de sondes et vaisseaux spaciaux'
+    ],
+    'naqadahreactor' => [
+        'name' => 'Reacteur au Naqahdah',
+        'description' => "Centrale permettant de générer d'énormes quantités d'énergie en consommant du Naqahdah."
+    ],
+    'ironstorage' => [
+        'name' => 'Entrepôt de Fer',
+        'description' => 'Permet de multiplier la capacité de stockage en Fer par 1.8 / LVL'
+    ],
+    'goldstorage' => [
+        'name' => "Entrepôt d'Or",
+        'description' => 'Permet de multiplier la capacité de stockage en Or par 1.8 / LVL'
+    ],
+    'quartzstorage' => [
+        'name' => 'Entrepôt de Quartz',
+        'description' => 'Permet de multiplier la capacité de stockage en Quartz par 1.8 / LVL'
+    ],
+    'naqahdahstorage' => [
+        'name' => 'Entrepôt de Naqadah',
+        'description' => 'Permet de multiplier la capacité de stockage en Naqadah par 1.8 / LVL'
+    ],
+    'defence' => [
+        'name' => 'Centre de défense',
+        'description' => "Permet à votre colonie de se défendre en cas d'attaques."
+    ],
+    'commandcenter' => [
+        'name' => 'Centre de commandement',
+        'description' => "Centre de commandement équipé d'une intelligence articifielle hors du commun. Votre vie sur cette colonie sera désormais bien plus aisée."
+    ],
+    'ironadvancedmine' => [
+        'name' => 'Mine de fer avancée',
+        'description' => "Désormais habitués à miner du fer sur cette colonie, vos volons ont dévellopés une manière bien plus éfficace d'extraire le fer de la planete"
+    ],
+    'goldadvancedmine' => [
+        'name' => "Mine d'or avancée",
+        'description' => "Désormais habitués à miner de l'or sur cette colonie, vos volons ont dévellopés une manière bien plus éfficace d'extraire l'or de la planete"
+    ],
+    'asuranfactory' => [
+        'name' => 'Asuran factory',
+        'description' => "Après de longues analyses, vos chercheurs réussissent à réactiver cette vieille usine Asuran de production d'E2PZ. vous ouvrant la voie vers le voyage interplanétaire"
+    ],
     'hiddenBuilding' => '-- Bâtiment caché --',
     'unDiscovered' => 'Non découvert',
     'unknownBuilding' => 'Bâtiment inconnu...',
