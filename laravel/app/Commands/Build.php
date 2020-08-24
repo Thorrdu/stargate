123a1e5459f73e88a2ed6277379e68bf03f50ba9
Thorrdu <thierry.hansens@gmail.com>
2020-08-24 19:43:10 +0200

    fix

--- laravel/app/Commands/Build.php
+++ laravel/app/Commands/Build.php
@@ -231,7 +231,7 @@ class Build extends CommandHandler implements CommandInterface
                             else
                             {
                                 if(count($this->args) == 2 && (int)$this->args[1] > 0)
-                                    $wantedLvl = (int)$this->args[2];
+                                    $wantedLvl = (int)$this->args[1];
 
                                 $buildingPrice = "";
                                 $buildingPrices = $building->getPrice($wantedLvl);
