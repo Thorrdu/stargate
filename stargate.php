eefdc483186ec97daf4ed271709408cc28f88038
Thorrdu <thierry.hansens@gmail.com>
2020-09-01 03:00:06 +0200

    fix

--- stargate.php
+++ stargate.php
@@ -89,12 +89,13 @@ use Carbon\Carbon;
 use Carbon\CarbonInterface;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
+use Discord\Parts\User\Activity;
 
 
 global $upTimeStart;
 $upTimeStart = Carbon::now();
 
-$beta = true;
+$beta = false;
 $token = 'NzMwODE1Mzg4NDAwNjE1NDU1.Xwc_Dg.9GJ5Mww-YtAeQZZ-2C9MR3EWn2c';
 $prefix = '!';
 
@@ -114,7 +115,12 @@ $discord = new DiscordCommandClient([
 $discord->on('ready', function ($discord) use($beta){
     echo "Bot is starting up!", PHP_EOL;
 
-    echo $discord->users->count();
+    $activity = $discord->factory(\Discord\Parts\User\Activity::class, [
+        'name' => "!help | {$discord->guilds->count()} servers {$discord->users->count()} users",
+        'type' => 3
+    ]);
+    $discord->updatePresence($activity);
+
     /*echo 'UPDATING PRESENCE'.PHP_EOL;
     $game = $discord->factory(Game::class, [
         'name' => "!help | {$discord->guilds->count()} servers {$discord->users->count()} users",
@@ -129,6 +135,8 @@ $discord->on('ready', function ($discord) use($beta){
 
 	// Listen for messages.
 	$discord->on('message', function ($message) {
+        if($message->author->user->bot == true)
+            return;
         if($message->guild_id != 735390211130916904 && $message->guild_id != 735390211130916904)
             return;
 		echo "{$message->author->user->username }: {$message->content}",PHP_EOL;
@@ -186,7 +194,6 @@ $discord->on('ready', function ($discord) use($beta){
         ]);
         $discord->updatePresence($game);*/
 
-        return;
         $dateNow = Carbon::now();
         $reminders = Reminder::where('reminder_date', '<', $dateNow->format("Y-m-d H:i:s"))->orderBy('player_id','asc')->get();
         $totalReminders = $reminders->count();
@@ -209,52 +216,30 @@ $discord->on('ready', function ($discord) use($beta){
                     {
                         $rmdMessagesStr .= $reminder->reminder;
 
-                        $userExist = $discord->users->get('id', $playerIdRemind);
-                        /*filter(function ($value) use($playerIdRemind){
-                            return $value->id == $playerIdRemind;
-                        })*/
-                        if($userExist->count() > 0)
-                        {
-                            $foundUser = $userExist->first();
-                            $foundUser->sendMessage($rmdMessagesStr);
-                        }
+                        $userExist = $discord->users->get('id',$playerIdRemind);
+                        if(!is_null($userExist))
+                            $userExist->sendMessage($rmdMessagesStr);
                     }
                     else
                     {
-                        $userExist = $discord->users->get('id', $playerIdRemind);
-                        if($userExist->count() > 0)
-                        {
-                            $foundUser = $userExist->first();
-                            $foundUser->sendMessage($rmdMessagesStr);
-                        }
+                        $userExist = $discord->users->get('id',$playerIdRemind);
+                        if(!is_null($userExist))
+                            $userExist->sendMessage($rmdMessagesStr);
 
                         $playerIdRemind = $reminder->player->user_id;
                         $rmdMessagesStr = $reminder->reminder;
 
-                        $userExist = $discord->users->get('id', $playerIdRemind);
-                        /*
-                        $userExist = $discord->users->filter(function ($value) use($playerIdRemind){
-                            return $value->id == $playerIdRemind;
-                        });*/
-                        if($userExist->count() > 0)
-                        {
-                            $foundUser = $userExist->first();
-                            $foundUser->sendMessage($rmdMessagesStr);
-                        }
+                        $userExist = $discord->users->get('id',$playerIdRemind);
+                        if(!is_null($userExist))
+                            $userExist->sendMessage($rmdMessagesStr);
                     }
                 }
                 else
                 {
-                    $userExist = $discord->users->get('id', $playerIdRemind);
-                    /*
-                    $userExist = $discord->users->filter(function ($value) use($playerIdRemind){
-                        return $value->id == $playerIdRemind;
-                    });*/
-                    if($userExist->count() > 0)
-                    {
-                        $foundUser = $userExist->first();
-                        $foundUser->sendMessage($rmdMessagesStr);
-                    }
+                    $userExist = $discord->users->get('id',$playerIdRemind);
+                    if(!is_null($userExist))
+                        $userExist->sendMessage($rmdMessagesStr);
+
                     $rmdMessagesStr = "";
                     $playerIdRemind = $reminder->player->user_id;
                 }
@@ -269,13 +254,10 @@ $discord->on('ready', function ($discord) use($beta){
         foreach($explorations as $exploration)
         {  
             $explorationOutcome = $exploration->outcome();
-            $userExist = $discord->users->filter(function ($value) use($exploration){
-                return $value->id == $exploration->player->user_id;
-            });
-            if($userExist->count() > 0)
+            $userExist = $discord->users->get('id',$exploration->player->user_id);
+            if(!is_null($userExist))
             {
-                $foundUser = $userExist->first();
-                $foundUser->sendMessage($explorationOutcome);
+                $userExist->sendMessage($explorationOutcome);
             }
         }
     });
@@ -354,7 +336,6 @@ $discord->on('ready', function ($discord) use($beta){
     ]);	
 
     $discord->registerCommand('stargate', function ($message, $args) use($discord){
-        return 'Under maintenance...';
         $command = new Stargate($message,$args,$discord);
         return $command->execute();
     },[
@@ -396,7 +377,6 @@ $discord->on('ready', function ($discord) use($beta){
     ]);	*/
 
     $discord->registerCommand('alliance', function ($message, $args) use($discord){
-        return 'Under maintenance...';
         $command = new AllianceCommand($message,$args,$discord);
         return $command->execute();
     },[
