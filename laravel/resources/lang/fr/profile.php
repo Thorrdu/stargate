8c58e9e2bd229b13a7a778612e94dd26e0abed04
Thorrdu <thierry.hansens@gmail.com>
2020-09-17 21:52:09 +0200

    fixes

--- laravel/resources/lang/fr/profile.php
+++ laravel/resources/lang/fr/profile.php
@@ -1,22 +1,18 @@
 <?php
 //PROFILE FR
 return [
-    "notification" => [
-        "missingParameter" => "Mauvais paramètres. essayez `!profile notification on/off`",
-        "disabled" => "Notification de Bâtiment/Recherche **Activée**",
-        "enabled" => "Notification de Bâtiment/Recherche **Désactivée**",
-        'nextVacation' => 'Vous êtes revenu de **mode vacance** trop récement. Vous pourrez le réactiver dans **:time**.',
-        'vacationUntil' => 'Vous avez activé le **mode vacance** trop récement. Vous pourrez le désactiver dans **:time**.',
-        'vacationConfirm' => 'Désirez-vous activer le **mode vacance** ?'.
-                            "\nAucun joueur ne pourra vous attaquer durant cette période mais vos mines ne produiront plus.".                     
-                            "\nAttention, vous ne pourrez le désactiver que 3 jours plus tard!",
-        'vacationActivated' => 'Mode vacance **Actif**, bonne vacances!',
-        'vacationOverConfirm' => 'Souhaitez-vous désactiver le **mode vacance** ?'.
-                                "\nAttention, vous ne pourrez l'activer à nouveau que 3 jours plus tard!",
-        'vacationOver' => 'Mode vacance **inactif**, bon retour!',
-        'vacationMode' => 'Mode vacance **Actif**. Pour le désactivé, utilisez `!profile vacation`.',
-        'playerVacation' => 'Action impossible, joueur en **mode vacance**.'
-
-
-    ]
+    "missingParameter" => "Mauvais paramètres. essayez `!profile notification on/off`",
+    "disabled" => "Notification de Bâtiment/Recherche **Activée**",
+    "enabled" => "Notification de Bâtiment/Recherche **Désactivée**",
+    'nextVacation' => 'Vous êtes revenu de **mode vacance** trop récement. Vous pourrez le réactiver dans **:time**.',
+    'vacationUntil' => 'Vous avez activé le **mode vacance** trop récement. Vous pourrez le désactiver dans **:time**.',
+    'vacationConfirm' => 'Désirez-vous activer le **mode vacance** ?'.
+                        "\nAucun joueur ne pourra vous attaquer durant cette période mais vos mines ne produiront plus.".                     
+                        "\nAttention, vous ne pourrez le désactiver que 3 jours plus tard!",
+    'vacationActivated' => 'Mode vacance **Actif**, bonne vacances!',
+    'vacationOverConfirm' => 'Souhaitez-vous désactiver le **mode vacance** ?'.
+                            "\nAttention, vous ne pourrez l'activer à nouveau que 3 jours plus tard!",
+    'vacationOver' => 'Mode vacance **inactif**, bon retour!',
+    'vacationMode' => 'Mode vacance **Actif**. Pour le désactivé, utilisez `!profile vacation`.',
+    'playerVacation' => 'Action impossible, joueur en **mode vacance**.'
 ];
