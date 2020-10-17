43d1b40a62200f08c9139ef48827bc4d65cba725
Thorrdu <thierry.hansens@gmail.com>
2020-10-17 19:48:36 +0200

    fix trade

--- laravel/app/Trade.php
+++ laravel/app/Trade.php
@@ -33,12 +33,18 @@ class Trade extends Model
 
     public function getFairness()
     {
-        if($this->playerSource->points_total > $this->playerDest->points_total && ($this->trade_value_player2/$this->trade_value_player1) > 1.25)
+        $diff1 = 1;
+        if($this->trade_value_player1 > 0)
+            $diff1 = $this->trade_value_player1;
+        $diff2 = 1;
+        if($this->trade_value_player1 > 0)
+            $diff2 = $this->trade_value_player2;
+        if($this->playerSource->points_total > $this->playerDest->points_total && ($this->trade_value_player2/$diff1) > 1.25)
         {
             //Si player 1 est plus fort et à donné plus de 15% en plus que player 2
             return false;
         }
-        elseif($this->playerSource->points_total < $this->playerDest->points_total && ($this->trade_value_player1/$this->trade_value_player2) > 1.25)
+        elseif($this->playerSource->points_total < $this->playerDest->points_total && ($this->trade_value_player1/$diff2) > 1.25)
         {
             //Si player 2 est plus fort et à donné plus de 15% en plus que player 1
             return false;
