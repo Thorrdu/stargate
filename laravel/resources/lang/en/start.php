2583f1e0610b9fd65ed6168e4dbf650128cc6221
Thorrdu <thierry.hansens@gmail.com>
2020-11-23 13:14:38 +0100

    fix en text

--- laravel/resources/lang/en/start.php
+++ laravel/resources/lang/en/start.php
@@ -6,9 +6,9 @@ return [
     "startMessage" => "This is how everything started,\n\n"
                       ."Thrilled to finally been chosen to join the SGC program and affected to Earth's new base on an distant planet.\n"
                       ."What no one anticipated was that at the time you crossed the Stargate to join your new assignement, an anomaly would appear on the way to the planet.\n"
-                      ."\nSo you're now alone on an unknown place and stars are non common with your good old Milky Way.\n"
-                      ."With you there is only a douzen men and women, a few supply crates and in sight just a shattered Stargate without any DHD. \n"
-                      ."Yopu were quickly named as the new leader of this unlucky group and you decided to explore this planet to finally found a new place to settle.\n"
+                      ."\nSo you're now alone on an unknown place and stars position seems completely different from what you're used to in your good old Milky Way.\n"
+                      ."With you there is only a dozen men and women, a few supply crates and in sight just a shattered Stargate without any DHD. \n"
+                      ."You were quickly named as the new leader of this unlucky group and you decided to explore this planet to finally found a new place to settle.\n"
                       ."As a leader on this new colony, your role is to develop and defend your place, explore the galaxy and find a way to go back on Earth.\n"
                       ."\nTo check your colony's information, use `!colony` (or `!c`) \nThen build your first buildings like an energy source with `!build` (or `!b`)".
                       "\n\nFor a more complete tutorial, check `!tutorial`".
